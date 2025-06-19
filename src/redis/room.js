b8c6a7e2773e80266755898862c752bceaa15b9e
lizhaohu <88468457+givemesomelove@users.noreply.github.com>
2025-06-19 18:23:46 +0800

    feat：完善房间ws逻辑

--- src/redis/room.js
+++ src/redis/room.js
@@ -1,6 +1,95 @@
 
 const redis = require('../../lib/lzhRedis/lzhRedis');
 
-const userRedis = {};
+const createRoomKey = (roomId) => {
+    return "room:" + roomId;
+};
 
-userRedis.createRoom
+const createRoomPlayersKey = (roomId) => {
+    return "room_players:" + roomId;
+};
+
+const userRedis = {};
+/// 创建房间
+userRedis.createRoom = async (userData) => {
+    if (!userData) throw new Error("用户信息缺失");
+
+    const roomId = `${userData._id}_${Date.now()}`;
+    const roomKey = createRoomKey(roomId);
+
+    const roomData = {
+        roomId,
+        ownerId: userData._id,
+        name: "新房间",
+        desc: "这是一个新创建的房间",
+        game: 0,
+        password: "",
+        maxPlayers: 8,
+        createAt: new Date().toISOString(),
+        updateAt: new Date().toISOString(),
+    };
+    /// 创建房间
+    await redis.setObject(roomKey, roomData);
+
+    /// 房间玩家相关
+    const roomPlayersKey = createRoomPlayersKey(roomId);
+    userData = { ...userData, ready: false };
+    await redis.setObject(roomPlayersKey, [userData]);
+
+    return roomId;
+}
+
+/// 玩家加入房间
+userRedis.joinRoom = async (roomId, userData) => {
+    if (!roomId || !userData) throw new Error("房间id或玩家信息异常");
+
+    /// 获取房间信息
+    const roomKey = createRoomKey(roomId);
+    const room = await redis.getObject(roomKey);
+    if (!room) {
+        throw new Error("房间不存在");
+    }
+
+    /// 获取房间玩家列表
+    const roomPlayersKey = createRoomPlayersKey(roomId);
+    const roomPlayers = await redis.getObject(roomPlayersKey);
+    if (!roomPlayers) {
+        throw new Error("房间玩家列表不存在");
+    }
+
+    /// 是否满员了
+    const maxPlayers = parseInt(room.maxPlayers, 10);
+    const playerCount = roomPlayers.length;
+    if (playerCount >= maxPlayers) throw new Error("房间已满员");
+
+    /// 加入房间
+    userData = { ...userData, ready: false };
+    roomPlayers.push(userData);
+    await redis.setObject(roomPlayersKey, roomPlayers);
+
+    return;
+}
+
+/// 清除当前所有房间
+userRedis.clearAllRooms = async () => {
+    const keys = await redis.client.keys("room:*");
+    const multi = redis.client.multi();
+    keys.forEach((key) => {
+        multi.del(key);
+    });
+    const playerKeys = await redis.client.keys("room_players:*");
+    playerKeys.forEach((key) => {
+        multi.del(key);
+    });
+    await multi.exec();
+}
+
+userRedis.getRoomList = async () => {
+    return await redis.getBatchObject("room:*");
+}
+
+userRedis.getRoomPlayers = async () => {
+    return await redis.getBatchObject("room_players:*");
+}
+
+module.exports = userRedis;
--- src/ws/room.js
+++ src/ws/room.js
@@ -1,10 +1,42 @@
 const LzhWS = require('../../lib/lzhWS/lzhWS');
+const roomRedis = require('../redis/room');
 class RoomWS extends LzhWS {
 
     initRouterMap() {
         /// 创建房间
         this.on('createRoom', async (socket, data) => {
-            console.log('create room', data);
+            const { userData } = data;
+            const roomId = await roomRedis.createRoom(userData);
+            /// 玩家ws加入房间
+            socket.join(roomId);
+            /// 创建成功
+            return { roomId };
+        })
+
+        /// 获取所有房间信息
+        this.on('getAllRooms', async (socket, data) => {
+            const roomList = await roomRedis.getRoomList();
+            const roomPlayers = await roomRedis.getRoomPlayers();
+            return { roomList, roomPlayers };
+        })
+
+        /// 加入房间
+        this.on('joinRoom', async (socket, data) => {
+            const { roomId, userData } = data;
+            /// redis加入房间
+            await roomRedis.joinRoom(roomId, userData);
+            /// 玩家ws加入房间
+            socket.join(roomId);
+            /// 通知房间内玩家房间信息更新
+            this.sendMessage2Room(roomId, 'roomUpdate', {});
+            return { roomId };
+        }) 
+
+        /// 清空房间
+        this.on('clearRooms', async (socket, data) => {
+            await roomRedis.clearAllRooms();
+            this.sendMessage2World('roomUpdate', {});
+            return {};
         })
     }
 }
